d87b9d24d895e05aa73bf17fb086fbe85b816d9d
marc <sed@protonmail.com>
2023-04-23 22:09:04 +0200

    add new address for social recovery module on testnet: goerli & optimism-goerli

--- src/utils/network.js
+++ src/utils/network.js
@@ -3,14 +3,14 @@ const initNetworksConfig = () => {
     0x5: {
       name: "Goerli",
       chainId: "5",
-      socialRecoveryModuleAddress: "0xCbf67d131Fa0775c5d18676c58de982c349aFC0b",
+      socialRecoveryModuleAddress: "0x831153c6b9537d0fF5b7DB830C2749DE3042e776",
       client: process.env.REACT_APP_GOERLI_RPC,
       blockExplorer: "https://goerli.etherscan.io/"
     },
     0x1a4: {
       name: "Optimism Goerli",
       chainId: "420",
-      socialRecoveryModuleAddress: "0xCbf67d131Fa0775c5d18676c58de982c349aFC0b",
+      socialRecoveryModuleAddress: "0x831153c6b9537d0fF5b7DB830C2749DE3042e776",
       client: process.env.REACT_APP_OPTIMISM_GOERLI_RPC,
       blockExplorer: "https://goerli-optimism.etherscan.io",
     },
