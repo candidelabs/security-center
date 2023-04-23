f3a4f53e153ae1c949595d9ec8873fff2fd67514
marc <sed@protonmail.com>
2023-04-23 22:11:44 +0200

    add new social recovery module for optimism

--- src/utils/network.js
+++ src/utils/network.js
@@ -1,23 +1,9 @@
 const initNetworksConfig = () => {
   return {
-    // 0x5: {
-    //   name: "Goerli",
-    //   chainId: "5",
-    //   socialRecoveryModuleAddress: "0xCbf67d131Fa0775c5d18676c58de982c349aFC0b",
-    //   client: process.env.REACT_APP_GOERLI_RPC,
-    //   blockExplorer: "https://goerli.etherscan.io/"
-    // },
-    // 0x1a4: {
-    //   name: "Optimism Goerli",
-    //   chainId: "420",
-    //   socialRecoveryModuleAddress: "0xCbf67d131Fa0775c5d18676c58de982c349aFC0b",
-    //   client: process.env.REACT_APP_OPTIMISM_GOERLI_RPC,
-    //   blockExplorer: "https://goerli-optimism.etherscan.io",
-    // },
     0xa: {
       name: "Optimism",
       chainId: "10",
-      socialRecoveryModuleAddress: "0x8fe391F70B6d3fe30920D9a91cd9ba34a2D9f6FB",
+      socialRecoveryModuleAddress: "0xbc1920b63F35FdeD45382e2295E645B5c27fD2DA",
       client: process.env.REACT_APP_OPTIMISM,
       blockExplorer: "https://optimism.etherscan.io",
     },
