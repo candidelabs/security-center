ded85313d4f405e6bc3f383fabc042156394a5c6
marc <sed@protonmail.com>
2023-04-24 19:00:25 +0200

    fix etherscan url

--- src/utils/network.js
+++ src/utils/network.js
@@ -5,7 +5,7 @@ const initNetworksConfig = () => {
       chainId: "10",
       socialRecoveryModuleAddress: "0xbc1920b63F35FdeD45382e2295E645B5c27fD2DA",
       client: process.env.REACT_APP_OPTIMISM,
-      blockExplorer: "https://optimism.etherscan.io",
+      blockExplorer: "https://optimistic.etherscan.io",
     },
   };
 };
